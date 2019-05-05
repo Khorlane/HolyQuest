4e6494ee1bbf99629cc9a9c628be8c4450ab50ee
Steve Bryant <khorlane@yahoo.com>
2019-05-05 00:51:13 -0400

    First commit

---
+++ Server/main.swift
@@ -0,0 +1,78 @@
+//
+//  main.swift
+//  HolyQuest
+//
+//  Created by Stephen Bryant on 5/5/19.
+//  Copyright © 2019 CodePlain. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+class EchoServer {
+  let bufferSize = 1024
+  let port: Int
+  var listenSocket: Socket? = nil
+  var connected = [Int32: Socket]()
+  var acceptNewConnection = true
+
+  init(port: Int) {
+    self.port = port
+  }
+
+  deinit {
+    for socket in connected.values {
+      socket.close()
+    }
+    listenSocket?.close()
+  }
+
+  func start() throws {
+    let socket = try Socket.create()
+    listenSocket = socket
+    try socket.listen(on: port)
+    print("Listening port: \(socket.listeningPort)")
+    repeat {
+      let connectedSocket = try socket.acceptClientConnection()
+      print("Connection from: \(connectedSocket.remoteHostname)")
+      newConnection(socket: connectedSocket)
+    } while acceptNewConnection
+  }
+
+  func newConnection(socket: Socket)
+  {
+    connected[socket.socketfd] = socket
+    var cont = true
+    var dataRead = Data(capacity: bufferSize)
+    repeat {
+      do {
+        let bytes = try socket.read(into: &dataRead)
+        if bytes > 0 {
+          if let readStr = String(data: dataRead, encoding: .utf8) {
+            print("Received: \(readStr)")
+            try socket.write(from: readStr)
+            if readStr.hasPrefix("quit") {
+              cont = false
+              socket.close()
+            }
+            dataRead.count = 0
+          }
+        }
+      } catch let error {
+        print("error: \(error)")
+      }
+    } while cont
+    connected.removeValue(forKey: socket.socketfd)
+    socket.close()
+  }
+}
+
+let server = EchoServer(port: 7777)
+do
+{
+  try server.start()
+} catch let error
+{
+  print("Error: \(error)")
+}
