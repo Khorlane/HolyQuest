8aaab23ea9c9db3f466afe838c725a9e713f391c
Khorlane <khorlane@yahoo.com>
2019-08-22 17:34:34 -0400

    Update main.swift
    Trivial

--- Server/main.swift
+++ Server/main.swift
@@ -2,9 +2,9 @@
 // main.swift
 // The main()
 // Created by Steve Bryant on 12/25/2018.
-// Copyright 2019 Steve Bryant. All rights reserved.x
+// Copyright 2019 Steve Bryant. All rights reserved.
 
-import Foundation     // Not required at this timex
+import Foundation     // Not required at this time
 
 Initialization()
 
